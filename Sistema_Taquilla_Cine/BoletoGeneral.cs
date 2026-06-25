9b3bf42ab31e89434749d841667f983944df3e70
juniorH <ch7435557@gmail.com>
2026-06-24 18:48:11 -0600

    esta es la clase de boleto general

---
+++ Sistema_Taquilla_Cine/BoletoGeneral.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Taquilla_Cine
+{
+        public class BoletoGeneral : Boleto
+        {
+            public BoletoGeneral(decimal precioBase)
+                : base(precioBase)
+            {
+            }
+
+            public override decimal CalcularPrecioFinal()
+            {
+                return PrecioBase;
+            }
+        }
+    }
+
+
