7267218422d3710d2284e53e6ef08b34aa2dd64a
leopoldo <leopoldoalvaradoantonio@gmail.com>
2026-06-24 18:54:26 -0600

    este es el apartado del adulto mayor

---
+++ Sistema_Taquilla_Cine/BoletoAdultoMayor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Taquilla_Cine
+{
+    public class BoletoAdultoMayor : Boleto
+    {
+        // Atributo específico requerido
+        public string NumCredencialInapam { get; set; }
+
+        // Constructor
+        public BoletoAdultoMayor(decimal precioBase, string numCredencialInapam)
+            : base(precioBase)
+        {
+            NumCredencialInapam = numCredencialInapam;
+        }
+
+        // Aplica descuento del 50%
+        public override decimal CalcularPrecioFinal()
+        {
+            return PrecioBase * 0.50m;
+        }
+    }
+}
---
+++ Sistema_Taquilla_Cine/Boletos_AdultosMayores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Taquilla_Cine
+{
+    internal class Boletos_AdultosMayores
+    {
+    }
+}
