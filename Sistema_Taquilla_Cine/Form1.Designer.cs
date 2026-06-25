c3753854d30d314d7c062abd7ca6bf44f6ad5119
JohnKarolFloresHernandez <johnkarolfloreshernandez@gmail.com>
2026-06-24 22:32:41 -0600

    Agregue el documento PDF y modifique el diseño y rectifique que todo este correcto

--- Sistema_Taquilla_Cine/Form1.Designer.cs
+++ Sistema_Taquilla_Cine/Form1.Designer.cs
@@ -21,6 +21,9 @@
             this.btnCalcular = new System.Windows.Forms.Button();
             this.lblResultado = new System.Windows.Forms.Label();
             this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblDescuento = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTitulo
@@ -59,11 +62,11 @@
             // 
             // btnCalcular
             // 
-            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnCalcular.BackColor = System.Drawing.Color.Lime;
             this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.btnCalcular.ForeColor = System.Drawing.Color.White;
-            this.btnCalcular.Location = new System.Drawing.Point(130, 110);
+            this.btnCalcular.Location = new System.Drawing.Point(214, 116);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(150, 40);
             this.btnCalcular.TabIndex = 3;
@@ -86,13 +89,41 @@
             this.txtResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
             this.txtResultado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
-            this.txtResultado.Location = new System.Drawing.Point(130, 172);
+            this.txtResultado.Location = new System.Drawing.Point(33, 127);
             this.txtResultado.Name = "txtResultado";
             this.txtResultado.ReadOnly = true;
             this.txtResultado.Size = new System.Drawing.Size(150, 29);
             this.txtResultado.TabIndex = 5;
-            this.txtResultado.Text = "$0.00";
+            this.txtResultado.Text = "$80.00";
             this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblDescuento
+            // 
+            this.lblDescuento.AutoSize = true;
+            this.lblDescuento.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescuento.Location = new System.Drawing.Point(30, 213);
+            this.lblDescuento.Name = "lblDescuento";
+            this.lblDescuento.Size = new System.Drawing.Size(95, 18);
+            this.lblDescuento.TabIndex = 6;
+            this.lblDescuento.Text = "Descuento";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(406, 128);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(30, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Precio Actual Boleto";
             // 
             // Form1
             // 
@@ -100,6 +131,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(400, 240);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.lblDescuento);
             this.Controls.Add(this.txtResultado);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.btnCalcular);
@@ -120,6 +154,9 @@
         private System.Windows.Forms.Button btnCalcular;
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblDescuento;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Sistema_Taquilla_Cine/Form1.cs
+++ Sistema_Taquilla_Cine/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_Taquilla_CineWinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,24 +16,37 @@ namespace Sistema_Taquilla_Cine
         public Form1()
         {
             InitializeComponent();
+            lblDescuento.Visible = false;
         }
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
             Boleto boleto = null;
             decimal precioBase = 80m;
+            if (cmbTipoBoleto.SelectedItem == null)
+            {
+                MessageBox.Show("Selecciona un tipo de boleto");
+                return;
+            }
+
 
             if (cmbTipoBoleto.SelectedItem.ToString() == "Estudiante")
             {
                 boleto = new BoletoEstudiante(precioBase, "2026001");
+                lblDescuento.Text = "Descuento 30%";
+                lblDescuento.Visible = true;
             }
             else if (cmbTipoBoleto.SelectedItem.ToString() == "Adulto Mayor")
             {
                 boleto = new BoletoAdultoMayor(precioBase, "INAPAM123");
+                lblDescuento.Text = "Descuento 50%";
+                lblDescuento.Visible = true;
             }
             else if (cmbTipoBoleto.SelectedItem.ToString() == "General")
             {
                 boleto = new BoletoGeneral(precioBase);
+                lblDescuento.Text = "Descuento 0%";
+                lblDescuento.Visible = true;
             }
 
             if (boleto != null)
