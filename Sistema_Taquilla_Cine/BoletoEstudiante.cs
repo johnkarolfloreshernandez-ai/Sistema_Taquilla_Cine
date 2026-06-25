c056ca03e6cc67c02d321ac7fe27158f474f603a
Aida Sujey Carrillo Hernández <aidasujeycarrillohernandez1@gmail.com>
2026-06-24 18:31:33 -0600

    realize la clase de estudiantes

---
+++ Sistema_Taquilla_Cine/BoletoEstudiante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Taquilla_Cine
+{
+    public class BoletoEstudiante : Boleto
+    {
+        public string Matricula { get; set; }
+
+        public BoletoEstudiante(decimal precioBase, string matricula)
+            : base(precioBase)
+        {
+            Matricula = matricula;
+        }
+
+        public override decimal CalcularPrecioFinal()
+        {
+            return PrecioBase * 0.70m;
+        }
+    }
+}
